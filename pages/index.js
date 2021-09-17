19cd7d2414efc34d286865629a68e7a6ad96703a
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-09-17 19:47:34 +0800

    rendering Dummy meets

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
-import Link from 'next/link';
-import { Fragment } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY = [
+    {
+        id: 'ID1',
+        title: 'NextJS',
+        image: 'https://miro.medium.com/max/800/1*bc9pmTiyKR0WNPka2w3e0Q.png',
+        address: 'San Mateo Rizal',
+        description: 'This is Roniel Description'
+    },
+    {
+        id: 'ID2',
+        title: 'REACTJS',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png',
+        address: 'Rizal',
+        description: 'This is Roniel2 Description'
+    }
+]
 
 function index(){
     return(
-        <Fragment>
-            <h1>This is Home page</h1>
-            <ul>
-                <li>
-                    <Link href="/meet">Go to Second Pages</Link>
-                </li>
-            </ul>
-        </Fragment>
+        <MeetupList meetups={DUMMY} />
     );
 }
 
