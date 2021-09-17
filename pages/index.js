d169d92acb8b9b16ea49de142be5f1f3e3dc64be
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-09-17 15:34:32 +0800

    connecting two page (Practice)

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,11 @@ function index(){
     return(
         <Fragment>
             <h1>This is Home page</h1>
+            <ul>
+                <li>
+                    <Link href="/meet">Go to Second Pages</Link>
+                </li>
+            </ul>
         </Fragment>
     );
 }
--- pages/meet/index.js
+++ pages/meet/index.js
@@ -1,2 +1,12 @@
 // localhost:3000/meet
+import { Fragment } from "react";
 
+function meet(){
+    return(
+        <Fragment>
+            <h1>Second Page</h1>
+        </Fragment>
+    );
+}
+
+export default meet;
