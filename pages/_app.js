b3e914cdcd7bb309a64ddc84dc14e689ee52563e
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-09-18 14:24:28 +0800

    wrapper components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import Layout from '../components/layout/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <center><h2>Content Here</h2></center>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp
