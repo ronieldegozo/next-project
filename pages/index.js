c4772a0ebf0266f541ae5a83e4b21fa760dd3b5b
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-09-19 13:52:58 +0800

    pushing router for a specific ID

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY = [
     {
-        id: 'ID1',
+        id: 'id1',
         title: 'NextJS',
         image: 'https://miro.medium.com/max/800/1*bc9pmTiyKR0WNPka2w3e0Q.png',
         address: 'San Mateo Rizal',
         description: 'This is Roniel Description'
     },
     {
-        id: 'ID2',
+        id: 'id2',
         title: 'REACTJS',
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png',
         address: 'Rizal',
